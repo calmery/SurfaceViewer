154e7409e49cd09474689c5ca9cb929d8f07efe6
Marei Kikukawa <contact@marei.jp>
2018-12-11 12:36:44 +0900

    レイアウトを調整する

--- src/main/index.ts
+++ src/main/index.ts
@@ -5,11 +5,10 @@ import serve from "./serve";
 const launch = serve(path.resolve(__dirname, "../renderer"));
 
 app.on("ready", () => {
-  const browserWindow = new BrowserWindow();
-
-  // DevTools
-
-  browserWindow.webContents.openDevTools();
+  const browserWindow = new BrowserWindow({
+    width: 1024,
+    height: 768
+  });
 
   // Main
 
